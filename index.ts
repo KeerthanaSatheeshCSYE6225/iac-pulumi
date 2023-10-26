3a8762a55ae8e0317597323f64947dfbd54fd667
keerthana <lnu.ke@northeastern.edu>
2023-10-25 20:43:00 -0400

    rds

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
+import { Tag } from "@pulumi/aws/ec2";
 
 // Load configurations
 const config = new pulumi.Config("pulumicloud");
@@ -16,6 +17,10 @@ const vpcCidrBlock = config.require("vpcCidrBlock");
 
 const amiInstance = config.require("amiInstance");
 
+const keyPem = config.require("keyPem");
+const dialect = "mysql";
+const port = 3306;
+const publicSubnet = {};
 // Configure AWS provider with the specified region
 const provider = new aws.Provider("provider", {
   region: region,
@@ -162,6 +167,7 @@ const applicationSecurityGroup = new aws.ec2.SecurityGroup(
   {
     name: "applicationSecurityGroup",
     description: "Security group for EC2 instances hosting web applications",
+    vpcId: vpc.id,
     ingress: [
       {
         fromPort: 22,
@@ -191,27 +197,98 @@ const applicationSecurityGroup = new aws.ec2.SecurityGroup(
   },
   { provider }
 );
+const firstPublicSubnet = subnets[0].id;
 
 // Create an EC2 instance
+
+// Create DB Security Group
+const dbSecurityGroup = new aws.ec2.SecurityGroup("database-security-group", {
+  description: "Security group for RDS instances",
+  vpcId: vpc.id,
+  ingress: [
+    {
+      protocol: "tcp",
+      fromPort: 3306, // for MySQL/MariaDB, use 5432 for PostgreSQL
+      toPort: 3306,
+      securityGroups: [applicationSecurityGroup.id],
+    },
+  ],
+  egress: [
+    {
+      protocol: "tcp",
+      fromPort: 0,
+      toPort: 65535,
+      cidrBlocks: ["0.0.0.0/0"],
+    },
+  ],
+});
+
+export const privateSubnetIds = subnets.apply((subnets) =>
+  subnets.filter((_, index) => index % 2 !== 0).map((subnet) => subnet.id)
+);
+
+const dbSubnets = new aws.rds.SubnetGroup("dbsubnets", {
+  subnetIds: privateSubnetIds,
+});
+// RDS Parameter Group
+const rdsParameterGroup = new aws.rds.ParameterGroup("rds-parameter-group", {
+  family: "mysql8.0", // Change this to your specific DB engine and version
+  parameters: [
+    {
+      name: "max_user_connections",
+      value: "100", // Change this to your desired value
+    },
+  ],
+});
+
+// RDS Instance
+const rdsInstance = new aws.rds.Instance("csye6225-rds-instance", {
+  engine: "mysql",
+  //engineVersion: "8.0.33",
+  instanceClass: "db.t2.micro", // Change this to the desired instance type
+  allocatedStorage: 20, // Set the storage as needed
+  dbSubnetGroupName: dbSubnets.id, // Use your private subnet group name
+  multiAz: false,
+  parameterGroupName: rdsParameterGroup.name,
+  skipFinalSnapshot: true,
+  publiclyAccessible: false,
+  // name: "csye6225",
+  username: "csye6225",
+  password: "Kavu1234",
+  dbName: "csye6225",
+  vpcSecurityGroupIds: [dbSecurityGroup.id],
+  identifier: "csye6225-rds-instance",
+});
+
+// // User Data
+// const userData = pulumi.all([rdsInstance.endpoint]).apply(
+//   ([endpoint]) => `#!/bin/bash
+// echo "DB_USERNAME=csye6225" >> /etc/environment
+// echo "DB_PASSWORD=yourStrongPassword" >> /etc/environment
+// echo "DB_HOSTNAME=${endpoint}" >> /etc/environment`
+// );
+
 const ec2Instance = new aws.ec2.Instance(
   "ec2Instance",
   {
     ami: amiInstance,
     instanceType: "t2.micro",
+    keyName: keyPem,
     vpcSecurityGroupIds: [applicationSecurityGroup.id],
+    subnetId: firstPublicSubnet,
     rootBlockDevice: {
       volumeSize: 25,
       volumeType: "gp2",
       deleteOnTermination: true,
     },
-    ebsBlockDevices: [
-      {
-        deviceName: "/dev/sda1",
-        volumeSize: 25,
-        volumeType: "gp2",
-        deleteOnTermination: true,
-      },
-    ],
+    userData: pulumi.interpolate`#!/bin/bash
+    sudo echo "MYSQL_HOST=${rdsInstance.address}" | sudo tee /home/admin/webapp/.env
+    sudo echo "MYSQL_USER='${rdsInstance.username}'" | sudo tee -a /home/admin/webapp/.env
+    sudo echo "MYSQL_PASSWORD='${rdsInstance.password}'" | sudo tee -a /home/admin/webapp/.env
+    sudo echo "MYSQL_DATABASE='${rdsInstance.dbName}'" | sudo tee -a /home/admin/webapp/.env
+    sudo echo "MYSQL_DIALECT='${dialect}'" | sudo tee -a /home/admin/webapp/.env
+    echo 'Hello from the new EC2 instance';
+`,
   },
   { provider }
 );
@@ -221,11 +298,13 @@ export const vpcId = vpc.id;
 export const publicSubnetIds = subnets.apply((subnets) =>
   subnets.filter((_, index) => index % 2 === 0).map((subnet) => subnet.id)
 );
-export const privateSubnetIds = subnets.apply((subnets) =>
-  subnets.filter((_, index) => index % 2 !== 0).map((subnet) => subnet.id)
-);
+// export const privateSubnetIds = subnets.apply((subnets) =>
+//   subnets.filter((_, index) => index % 2 !== 0).map((subnet) => subnet.id)
+// );
 export const internetGatewayId = internetGateway.id;
 export const publicRouteTableId = publicRouteTable.id;
 export const privateRouteTableId = privateRouteTable.id;
 export const applicationSecurityGroupId = applicationSecurityGroup.id;
+
+export const rdsInstanceId = rdsInstance.id;
 export const ec2InstanceId = ec2Instance.id;
